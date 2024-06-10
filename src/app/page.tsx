ef3b159e7ec910a68b29896942438efd01059195
Magnewei <Magnewei@Icloud.com>
2024-06-10 20:38:58 +0200

    fixed array & list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,34 @@
+import ItemList from '@/components/ui/ItemList';
 import SiteHeader from '@/components/ui/siteheader';
 
-export const Weapon = [
+const sword = [
   {
-    excalibur: [
+    id: 1,
+    name: 'Excalibur',
+    materials: [
       {
+        id: 2,
+        name: 'hilt',
         materials: [
           {
-            swordHilt: [
-              { shaft: 'brown shaft' },
-              { bottomPiece: 'brown wooden piece' },
-            ],
-
-            swordBlade: [{ steel: 'dragonsteel' }, { polish: 'dragon tears' }],
+            id: 3,
+            name: 'brown shaft',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: 'Dragonbane Armor',
+    materials: [
+      {
+        id: 5,
+        name: 'Dragonbane chest',
+        materials: [
+          {
+            id: 6,
+            name: 'Dragonsteel Ingots',
           },
         ],
       },
@@ -19,17 +36,79 @@ export const Weapon = [
   },
 ];
 
-function createButtonForEach() {
-  // TODO: destructure weapon array, create button for each from top to bottom.
-  // TODO: add button component with styling etc. for each button.
-  // TOOD: Maybe make a list component to hold buttons?
-}
+const voidHighlord = [
+  {
+    id: 0,
+    name: 'Void Highlord',
+    materials: [
+      {
+        id: 0,
+        name: 'Roentgenium of Nulgath x15',
+        materials: [
+          {
+            id: 0,
+            name: "Void Highlord's Challenge",
+            link: 'http://aqwwiki.wikidot.com/void-highlord-s-quest',
+          },
+          {
+            id: 0,
+            name: "Nulgath Insignia x5 for 2 Roentgenium's (merge)",
+            link: '',
+          },
+        ],
+      },
+      {
+        id: 0,
+        name: 'Void Crystal A x1',
+        materials: [{}, {}],
+      },
+      {
+        id: 0,
+        name: 'Void Crystal B x1',
+        materials: [
+          {
+            id: 0,
+            name: 'Diamond of Nulgath x200',
+          },
+          {
+            id: 0,
+            name: 'Blood Gem of the Archfiend x30',
+          },
+          {
+            id: 0,
+            name: 'Totem of Nulgath x15',
+          },
+          {
+            id: 0,
+            name: "Elders' Blood x2",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: 'Dragonbane Armor',
+    materials: [
+      {
+        id: 5,
+        name: 'Dragonbane chest',
+        materials: [
+          {
+            id: 6,
+            name: 'Dragonsteel Ingots',
+          },
+        ],
+      },
+    ],
+  },
+];
 
-export function Home() {
+export default function Home() {
   return (
     <>
       <SiteHeader />
-      <div></div>
+      <ItemList items={voidHighlord} />
     </>
   );
 }
---
+++ src/components/ui/ItemList.tsx
@@ -0,0 +1,44 @@
+'use client';
+import './ItemList.css';
+
+import React, { useState } from 'react';
+
+export default function ItemList({ items }) {
+  const [displayChildren, setDisplayChildren] = useState({});
+
+  return (
+    <ul>
+      {items.map((item) => {
+        return (
+          <li key={item.name}>
+            {item.materials && (
+              <button
+                style={{
+                  color: 'yellow',
+                  backgroundColor: 'black',
+                  borderRadius: '20px',
+                  padding: '20px',
+                }}
+                onClick={() => {
+                  setDisplayChildren({
+                    ...displayChildren,
+                    [item.name]: !displayChildren[item.name],
+                  });
+                }}
+              >
+                {displayChildren[item.name] ? '-' : '+'}
+              </button>
+            )}
+            <span>
+              {' '}
+              <a href={item.link}>{item.name}</a>
+            </span>
+            {displayChildren[item.name] && item.materials && (
+              <ItemList items={item.materials} />
+            )}
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
